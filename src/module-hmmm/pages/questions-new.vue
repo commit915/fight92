fd22671bf72cbc7bb8c14f1218132f7e2632e35d
cp <1432737544@qq.com>
2020-02-06 15:50:40 +0800

    试题录入

--- src/module-hmmm/pages/questions-new.vue
+++ src/module-hmmm/pages/questions-new.vue
@@ -1,14 +1,213 @@
 <template>
   <div class="dashboard-container">
-    <div class="app-container">题库添加</div>
+    <el-card class="box-card">
+      <el-form ref="addFormRef" :model="addForm" label-width="200px">
+        <el-form-item label="学科：">
+          <el-select v-model="addForm.subjectID" placeholder="请选择">
+            <el-option
+              v-for="item in subjectIDList"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="目录：">
+          <el-select v-model="addForm.catalogID" placeholder="请选择">
+            <el-option
+              v-for="item in directoryList"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="企业：">
+          <el-select v-model="addForm.enterpriseID" placeholder="请选择">
+            <el-option
+              v-for="item in companyList"
+              :key="item.id"
+              :label="item.shortName"
+              :value="item.id"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="城市：">
+          <el-select
+            v-model="addForm.province"
+            placeholder="请选择"
+            @change="getCitysList(addForm.province)"
+          >
+            <el-option v-for="item in provinces()" :key="item" :value="item" :label="item"></el-option>
+          </el-select>
+          <el-select v-model="addForm.city" placeholder="请选择">
+            <el-option v-for="item in citysList" :key="item" :value="item" :label="item"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="方向：">
+          <el-select v-model="addForm.direction" placeholder="请选择">
+            <el-option
+              v-for="item in directionList"
+              :key="item"
+              :value="item"
+              :label="item"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="题型：">
+          <el-radio-group v-model="addForm.questionType">
+            <el-radio
+              v-for="item in questionTypeList"
+              :key="item.value"
+              :label="item.value+''"
+            >{{item.label}}</el-radio>
+          </el-radio-group>
+          </el-form-item>
+          <el-form-item label="难度：">
+          <el-radio-group v-model="addForm.difficulty">
+            <el-radio
+              v-for="item in difficultyList"
+              :key="item.value"
+              :label="item.value+''"
+            >{{item.label}}</el-radio>
+          </el-radio-group>
+        
+        </el-form-item>
+        <el-form-item label="题干：">
+          <el-input type="textarea" v-model="addForm.question"></el-input>
+        </el-form-item>
+        <el-form-item label="选项：">
+          <el-radio v-model="singleSelect" :label="0">
+            A:
+            <el-input type="text" v-model="addForm.options[0].title"></el-input>
+          </el-radio>
+          <br />
+          <el-radio v-model="singleSelect" :label="1">
+            B:
+            <el-input type="text" v-model="addForm.options[1].title"></el-input>
+          </el-radio>
+          <br />
+          <el-radio v-model="singleSelect" :label="2">
+            C:
+            <el-input type="text" v-model="addForm.options[2].title"></el-input>
+          </el-radio>
+          <br />
+          <el-radio v-model="singleSelect" :label="3">
+            D:
+            <el-input type="text" v-model="addForm.options[3].title"></el-input>
+          </el-radio>
+        </el-form-item>
+        <el-form-item label="答案：">
+          <el-input type="textarea" v-model="addForm.answer"></el-input>
+        </el-form-item>
+        <el-form-item label="备注：">
+          <el-input type="textarea" v-model="addForm.remarks"></el-input>
+        </el-form-item>
+        <el-form-item label="标签：">
+          <el-input type="text" v-model="addForm.tags"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="tianjia()">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <script>
+import { simple } from '@/api/hmmm/subjects.js'
+import { simple as directorysimple } from '@/api/hmmm/directorys.js'
+import { list } from '@/api/hmmm/companys.js'
+import { provinces, citys } from '@/api/hmmm/citys.js'
+import { add } from '@/api/hmmm/questions.js'
+
+import {
+  // 设置别名,方便后边使用
+  difficulty as difficultyList,
+  questionType as questionTypeList,
+  direction as directionList
+} from '@/api/hmmm/constants.js'
 export default {
   name: 'QuestionsNew',
   data() {
-    return {}
+    return {
+      directionList,
+      questionTypeList,
+      difficultyList,
+      subjectIDList: [],
+      directoryList: [],
+      companyList: [],
+      citysList: [],
+      singleSelect: '',
+      addForm: {
+        options: [
+          // {code: '编号ABCD', title: '当前项目文字答案',
+          //   img: '当前项目图片答案', isRight: boolean表明当前项目是否是答案},
+          { code: 'A', title: '', img: '', isRight: false },
+          { code: 'B', title: '', img: '', isRight: false },
+          { code: 'C', title: '', img: '', isRight: false },
+          { code: 'D', title: '', img: '', isRight: false }
+        ],
+        subjectID: '',
+        catalogID: '',
+        enterpriseID: '',
+        province: '',
+        city: '',
+        direction: '',
+        questionType: '1',
+        difficulty: '1',
+        question: '', // 题干
+        answer: '', // 答案
+        remarks: '', // 备注
+        tags: '', // 标签
+        videoURL: 'http://www.xxx.com' // 解析视频
+      }
+    }
+  },
+  watch: {
+    singleSelect(newV, oldV) {
+      for (var i = 0; i < 4; i++) {
+        this.addForm.options[i].isRight = false
+      }
+      this.addForm.options[newV].isRight = true
+    }
+  },
+  created() {
+    this.getSubjectIDList()
+    this.getDirectoryList()
+    this.getCompanyList()
+  },
+  methods: {
+    provinces,
+    getCitysList(province) {
+      // let result = await citys(province)
+      // console.log(province)
+      // console.log(result)
+      this.addForm.city = ''
+      this.citysList = citys(province)
+    },
+    async tianjia() {
+      let result = await add(this.addForm)
+      console.log(result)
+      this.$message.success('添加试题成功')
+      this.$router.push('/questions/list')
+      // this.subjectIDList = result.data
+    },
+    async getSubjectIDList() {
+      let result = await simple()
+      // console.log(result)
+      this.subjectIDList = result.data
+    },
+    async getDirectoryList() {
+      let result = await directorysimple()
+      // console.log(result)
+      this.directoryList = result.data
+    },
+    async getCompanyList() {
+      let result = await list()
+      console.log(result)
+      this.companyList = result.data.items
+    }
   }
 }
 </script>
--- src/module-hmmm/pages/questions.vue
+++ src/module-hmmm/pages/questions.vue
@@ -4,7 +4,7 @@
     <el-card class="box-card">
       <el-row class="row">
         <el-col>
-          <el-button type="info" plain>新增试题</el-button>
+          <el-button type="info" plain @click="$router.push('/questions/new')">新增试题</el-button>
           <el-button type="info" plain>批量导入</el-button>
         </el-col>
       </el-row>
@@ -57,14 +57,15 @@
       <el-row :gutter="20" class="row">
         <el-col :span="6">
           城市
-          <el-select v-model="searchForm.province" @change="getCitysList">
-            <el-option v-for="(val,index) in provincesList" :key="index" :value="val" :label="val"></el-option>
+          <el-select v-model="searchForm.province" @change="getCitysList(searchForm.province)">
+            <!-- <el-option v-for="(val,index) in provinces()" :key="index" :value="val" :label="val"></el-option> -->
+            <el-option v-for="item in provinces()" :key="item" :value="item" :label="item"></el-option>
           </el-select>
         </el-col>
         <el-col :span="6">
           区
           <el-select v-model="searchForm.city">
-            <el-option v-for="(val,index) in citysList" :key="index" :value="index" :label="val"></el-option>
+            <el-option v-for="item in citysList" :key="item" :value="item" :label="item"></el-option>
           </el-select>
         </el-col>
         <el-col :span="6">
@@ -105,57 +106,71 @@
         </el-col>
         <el-col :span="6">
           二级目录
-          <el-input v-model="searchForm.catalogID" placeholder="请输入" style="width:210px"></el-input>
+          <el-select v-model="searchForm.catalogID">
+            <el-option
+              v-for="item in directoryList"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
         </el-col>
       </el-row>
       <el-row class="anniu row">
         <el-col>
           <el-button @click="deleteItem()">清除</el-button>
-          <el-button type="primary">搜索</el-button>
+          <el-button type="primary" @click="getQuestionList()">搜索</el-button>
         </el-col>
       </el-row>
       <el-table :data="baseList" border style="width: 100%">
-        <el-table-column prop="id" label="序号"></el-table-column>
+        <el-table-column type="index" label="序号"></el-table-column>
         <el-table-column prop="number" label="试题编号" width="120"></el-table-column>
-        <el-table-column prop="subjectID" label="学科" width="120"></el-table-column>
-        <el-table-column prop="questionType" label="题型" width="120"></el-table-column>
+        <el-table-column prop="subject" label="学科" width="120"></el-table-column>
+        <el-table-column :formatter="questionTypeFMT" prop="questionType" label="题型" width="120"></el-table-column>
         <el-table-column prop="question" label="题干" width="120"></el-table-column>
-        <el-table-column prop="addDate" label="录入时间" width="120"></el-table-column>
-        <el-table-column prop="difficulty" label="难度" width="120"></el-table-column>
-        <el-table-column prop="number" label="使用次数" width="120"></el-table-column>
+        <el-table-column label="录入时间" width="120">
+          <span slot-scope="stData">{{stData.row.addDate | parseTimeByString}}</span>
+        </el-table-column>
+        <el-table-column :formatter="difficultyFMT" prop="difficulty" label="难度" width="120"></el-table-column>
         <el-table-column prop="creator" label="录入人" width="120"></el-table-column>
         <el-table-column label="操作" width="200">
-          <!-- <template slot-scope="scope"> -->
-          <el-button @click="handleClick(scope.row)" type="text" size="small">预览</el-button>
-          <el-button type="text" size="small">修改</el-button>
-          <el-button type="text" size="small">删除</el-button>
-          <el-button type="text" size="small">加入精选</el-button>
-          <!-- </template> -->
+          <!-- 在外边加个template,一次性给所有a标签起作用，简化 -->
+          <!-- el-table-column为子组件，利用作用域插槽slot-scope="stData" -->
+          <template slot-scope="stData">
+            <!-- <el-button type="text" size="small">预览</el-button>
+            <el-button type="text" size="small">修改</el-button>
+            <el-button type="text" size="small" @click="del(stData.row)">删除</el-button>
+            <el-button type="text" size="small">加入精选</el-button> -->
+            <!-- 这里如果用a标签，因为a标签会自动跳转，导致还未执行click事件就已经跳转，用修饰符.prevent阻止其默认行为 -->
+            <a href="#">预览</a>
+            <a href="#">修改</a>
+            <a href="#"  @click.prevent="del(stData.row)">删除</a>
+            <a href="#">加入精选</a>
+          </template>
         </el-table-column>
       </el-table>
-      <div class="block">
+      <!-- <div class="block">
         <span class="demonstration"></span>
         <el-pagination
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          :current-page="currentPage4"
+          :current-page="searchForm.page"
           :page-sizes="[10, 20, 30, 40]"
-          :page-size="10"
-          layout="total, sizes, prev, pager, next, jumper"
-          :total="400"
+          :page-size="searchForm.pagesize"
+          total="pageList.counts"
         ></el-pagination>
-      </div>
+      </div>-->
     </el-card>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
+
 import { simple } from '@/api/hmmm/subjects.js'
 import { simple as tagsimple } from '@/api/hmmm/tags.js'
 import { simple as usersimple } from '@/api/base/users.js'
+import { simple as directorysimple } from '@/api/hmmm/directorys.js'
 import { provinces, citys } from '@/api/hmmm/citys.js'
-import { list } from '@/api/hmmm/questions.js'
+import { list, remove } from '@/api/hmmm/questions.js'
 
 import {
   // 设置别名,方便后边使用
@@ -173,11 +188,18 @@ export default {
       directionList,
       subjectIDList: [],
       tagsList: [],
-      provincesList: [],
+      // provincesList: [],
       citysList: [],
       usersList: [],
       baseList: [],
+      directoryList: [],
+      pageList: [],
       searchForm: {
+        page: 1,
+        pages: 1,
+        pagesize: 10,
+        count: 8,
+        // addData: '',
         subjectID: '',
         difficulty: '',
         questionType: '',
@@ -193,25 +215,99 @@ export default {
       }
     }
   },
+  watch: {
+    searchForm: {
+      handler: function(newV, oldV) {
+        this.getList()
+      },
+      deep: true
+    }
+  },
   created() {
     this.getSubjectIDList()
     this.getTagsList()
-    this.getProvincesList()
-    // this.getCitysList()
+    // this.getProvincesList()
+    this.getCitysList()
     this.getUsersList()
     this.getList()
+    this.getDirectoryList()
   },
+  // watch: {
+  //   'searchForm.province': function() {
+  //     this.searchForm.city = ''
+  //     this.citysList = citys()
+  //   }
+  // },
   methods: {
+      del(question) {
+       this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(async() => {
+          // 加async,await為了保證先刪除成功之後,才刷新頁面
+          let result = await remove(question)
+          console.log(result)
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          })
+          // 刷新頁面
+          this.getQuestionList()
+
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })          
+        })
+     
+    },
+    async getQuestionList() {
+      let result = await list(this.searchForm)
+      // console.log(result)
+      this.baseList = result.data.items
+    },
+     difficultyFMT(row, column, cellValue, index) {
+      // return cellValue
+      // console.log(index)
+      return this.difficultyList[cellValue - 1].label
+    },
+    questionTypeFMT(row, column, cellValue, index) {
+      // return cellValue
+      // console.log(cellValue)
+      return this.questionTypeList[cellValue - 1].label
+    },
+    // 可以在methods中直接声明citys.js中的provinces()方法
+    provinces,
+     // async getProvincesList() {
+    //   let result = await provinces()
+    //   // console.log(result)
+    //   this.provincesList = result
+    // },
+    getCitysList(province) {
+      // let result = await citys(province)
+      // console.log(province)
+      // console.log(result)
+      this.searchForm.city = ''
+      this.citysList = citys(province)
+    },
+    async getDirectoryList() {
+      let result = await directorysimple()
+      // console.log(result)
+      this.directoryList = result.data
+    },
     deleteItem() {
       for (let k of Object.keys(this.searchForm)) {
-        console.log(Object.keys(this.searchForm))
+        // console.log(Object.keys(this.searchForm))
         Vue.delete(this.searchForm, k)
       }
     },
     async getList() {
       let result = await list()
-      // console.log(result)
+      console.log(result)
       this.baseList = result.data.items
+      this.pageList = result.data
     },
     async getSubjectIDList() {
       let result = await simple()
@@ -223,17 +319,6 @@ export default {
       // console.log(result)
       this.tagsList = result.data
     },
-    async getProvincesList() {
-      let result = await provinces()
-      // console.log(result)
-      this.provincesList = result
-    },
-    async getCitysList(province) {
-      let result = await citys(province)
-      // console.log(province)
-      // console.log(result)
-      this.citysList = result
-    },
     async getUsersList() {
       let result = await usersimple()
       // console.log(result)
@@ -244,13 +329,13 @@ export default {
 </script>
 
 <style scoped>
-.row{
+.row {
   margin-bottom: 20px;
 }
 .anniu {
   margin-left: 1070px;
 }
-.block{
+.block {
   margin-left: 700px;
   margin-top: 30px;
 }
